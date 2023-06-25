c11f89d53794b007dc3066203fa9d742a9542c87
Sanechek1337 <rednaxela659@gmail.com>
2023-06-25 18:04:09 +0600

    added skeleton and some routes

--- src/App.jsx
+++ src/App.jsx
@@ -1,55 +1,27 @@
 import { useState, useEffect } from 'react';
+import { Route, Routes } from 'react-router-dom';
 
 import Header from './components/Header';
-import Categories from './components/Categories';
-import Sort from './components/Sort';
-import PizzaBlock from './components/PizzaBlock';
+import Home from './pages/Home';
+import Cart from './pages/Cart';
+import NotFound from './pages/NotFound';
 
 import './scss/app.scss';
 
 function App() {
-	const [pizzaList, setPizzaList] = useState([]);
-
-	useEffect(() => {
-		// (async () => {
-		// 	const request = await fetch('https://64932246428c3d2035d1632a.mockapi.io/pizzasData/pizzas')
-		// 		.then(res => res.json())
-
-		// 	if (request) {
-		// 		setPizzaList(request)
-		// 	}
-		// })()
-
-		fetch('https://64932246428c3d2035d1632a.mockapi.io/pizzasData/pizzas')
-			.then(res => res.json())
-			.then(pizzas => setPizzaList(pizzas))
-
-	}, [])
-
 	return (
 		<div className="wrapper">
 			<Header />
 			<div className="content">
 				<div className="container">
-					<div className="content__top">
-						<Categories />
-						<Sort />
-					</div>
-					<h2 className="content__title">Все пиццы</h2>
-					<div className="content__items">
-						{
-							pizzaList.map((pizzaInfo, index) => {
-								return (
-									<PizzaBlock
-										{...pizzaInfo}
-										key={index}
-									/>
-								)
-							})
-						}
-					</div>
+					<Routes>
+						<Route path='/' element={<Home />} />
+						<Route path='/cart' element={<Cart />} />
+						<Route path='*' element={<NotFound />} />
+					</Routes>
 				</div>
 			</div>
+
 		</div>
 	);
 }
---
+++ src/pages/Home.jsx
@@ -0,0 +1,57 @@
+import { useState, useEffect } from 'react';
+
+import Categories from '../components/Categories';
+import Sort from '../components/Sort';
+import PizzaBlock from '../components/PizzaBlock';
+import { PizzaSkeleton } from '../components/PizzaBlock/PizzaSkeleton';
+
+const Home = () => {
+	const [pizzaList, setPizzaList] = useState([]);
+	const [isLoading, setIsLoading] = useState(true);
+
+	useEffect(() => {
+		// (async () => {
+		// 	const request = await fetch('https://64932246428c3d2035d1632a.mockapi.io/pizzasData/pizzas')
+		// 		.then(res => res.json())
+
+		// 	if (request) {
+		// 		setPizzaList(request)
+		// 	}
+		// })()
+
+		fetch('https://64932246428c3d2035d1632a.mockapi.io/pizzasData/pizzas')
+			.then(res => res.json())
+			.then(pizzas => {
+				setPizzaList(pizzas);
+				setIsLoading(false);
+			})
+	}, [])
+
+	return (
+		<>
+			<div className="content__top">
+				<Categories />
+				<Sort />
+			</div>
+			<h2 className="content__title">Все пиццы</h2>
+			<div className="content__items">
+				{
+					isLoading
+						? [...new Array(10)].map((_, index) => {
+							return <PizzaSkeleton key={index} />
+						})
+						: pizzaList.map((pizzaInfo, index) => {
+							return (
+								<PizzaBlock
+									{...pizzaInfo}
+									key={pizzaInfo.id}
+								/>
+							)
+						})
+				}
+			</div>
+		</>
+	);
+}
+
+export default Home;
